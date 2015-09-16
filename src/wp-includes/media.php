8313cb20405f1038fbecb2fdd3fee74091522a46
Joe McGill <joemcgill@gmail.com>
2015-09-15 20:19:41 -0500

    Improve doc description for `wp_attachment_img_srcset_array()`

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -832,7 +832,8 @@ function wp_get_attachment_image_url( $attachment_id, $size = 'thumbnail', $icon
 }
 
 /**
- * Get an array of image sources candidates for use in a 'srcset' attribute.
+ * Return an array of urls and pixel widths representing sizes of an image for
+ * the purpose of populating a source set when creating responsive image markup.
  *
  * @since 4.4.0
  *
