80080e4a1507bd42a6f48e74cc78287e2f007b07
Jasper de Groot <mail@ugomobi.com>
2015-10-12 00:04:27 +0200

    Correction in description

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -833,7 +833,7 @@ function wp_get_attachment_image_url( $attachment_id, $size = 'thumbnail', $icon
  *
  * @param  int    $attachment_id   Image attachment ID.
  * @param  string $size            Optional. Name of image size. Default value: 'medium'.
- * @return array|bool $images {
+ * @return array|bool $sources {
  *     Array image candidate values containing a url, descriptor type, and
  *     descriptor value. False if none exist.
  *
